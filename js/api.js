baedf0359361a285c9de13cda40df7ca8546b586
Molly Wilson <molly_wilson1@outlook.com>
2021-02-19 12:06:59 -0600

    Change api call to https

--- js/api.js
+++ js/api.js
@@ -1,8 +1,8 @@
 
 export function getMovieById(id, callback) {
-    $.get("http://www.omdbapi.com/?i=" + id + "&apikey=d4428604", callback);
+    $.get("https://www.omdbapi.com/?i=" + id + "&apikey=d4428604", callback);
 }
 
 export function getMovieByIdFullPlot(id, callback) {
-    $.get("http://www.omdbapi.com/?i=" + id + "&apikey=d4428604&plot=full", callback);
+    $.get("https://www.omdbapi.com/?i=" + id + "&apikey=d4428604&plot=full", callback);
 }
--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-$.get("http://www.omdbapi.com/?i=tt3896198&apikey=d4428604", function(data,status){
+$.get("https://www.omdbapi.com/?i=tt3896198&apikey=d4428604", function(data,status){
     $(".carousel-item:first").css("background-image", "url(" + data.Poster + ")");
 });
 
-// $.get("http://www.omdbapi.com/?i=tt0120338&apikey=d4428604", function(data,status){
+// $.get("https://www.omdbapi.com/?i=tt0120338&apikey=d4428604", function(data,status){
 //     console.log($(".card-img:first"));
 //     $(".card-img-top:first").attr("src", data.Poster);
 // });
@@ -10,7 +10,7 @@ $.get("http://www.omdbapi.com/?i=tt3896198&apikey=d4428604", function(data,statu
 var movies = ["tt0120338", "tt1291150", "tt0172495", "tt0128853", "tt7547410", "tt3896198"];
 
 for(var i = 0; i < 6; i++){
-    $.get("http://www.omdbapi.com/?i=" + movies[i] + "&apikey=d4428604", function(data, status){
+    $.get("https://www.omdbapi.com/?i=" + movies[i] + "&apikey=d4428604", function(data, status){
         var index = movies.indexOf(data.imdbID); 
         var portfolio = $(".portfolio-item").eq(index);
         
