7c10565acde96c1737f6c85958e17c4d5d5786bf
Molly Wilson <molly_wilson1@outlook.com>
2021-02-21 20:57:18 -0600

    Handle no search results found case

--- js/search.js
+++ js/search.js
@@ -3,32 +3,41 @@ import { search, getMovieById, getMovieByIdFullPlot } from "./api.js";
 var urlParams = new URLSearchParams(window.location.search);
 
 search(urlParams.get("search"), function (data) {
-    $("#result-count").append(data.Search.length)
-    
-    $("#search").text("\"" + urlParams.get("search") + "\"");
-    console.log(data);
 
-    for(let result of data.Search) {
-        //jquery magic look up later. IT JUST WORKS
-        var template = $($("#search-result-template").html());
-
-        template.find(".movie-title").text(result.Title);
-        template.find(".result-year").text(result.Year);
-        template.find(".btn").attr("href", "movie.html?id=" + result.imdbID);
-
-        if(result.Type == "series"){
-            template.find(".result-info").append("(TV Series)");
-        }
-
-        if(result.Poster == "N/A" || result.Poster == ""){
-            template.find(".poster").attr("src", "https://www.tu-chemnitz.de/physik/TPSM/bilder/noimageavailable.png");
-        }
-        else {
-            template.find(".poster").attr("src", result.Poster);
-        }
-        
-        $("#search-results").append(template);
+    if (data.Response == "False") {
+        $("h1").text("No results matching \"" + urlParams.get("search") + "\" found")
     }
+
+    else {
+        $("#result-count").append(data.Search.length)
+
+        $("#search").text("\"" + urlParams.get("search") + "\"");
+        console.log(data);
+
+        for (let result of data.Search) {
+            //jquery magic look up later. IT JUST WORKS
+            var template = $($("#search-result-template").html());
+
+            template.find(".movie-title").text(result.Title);
+            template.find(".result-year").text(result.Year);
+            template.find(".btn").attr("href", "movie.html?id=" + result.imdbID);
+
+            if (result.Type == "series") {
+                template.find(".result-info").append("(TV Series)");
+            }
+
+            if (result.Poster == "N/A" || result.Poster == "") {
+                template.find(".search-poster").attr("src", "https://www.tu-chemnitz.de/physik/TPSM/bilder/noimageavailable.png");
+            }
+            else {
+                template.find(".search-poster").attr("src", result.Poster);
+            }
+
+            $("#search-results").append(template);
+        }
+    }
+
+
 
 });
 
