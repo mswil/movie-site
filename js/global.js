18caa660185b62ab9e81c17265bfe093a2b72f2f
Molly Wilson <molly_wilson1@outlook.com>
2021-02-25 13:22:17 -0600

    Fixed toast visibility bug

--- js/global.js
+++ js/global.js
@@ -1,5 +1,9 @@
 $('.toast').toast({autohide:true, delay:2000});
+$(".toast").hide();
 
+$('.toast').on('hidden.bs.toast', function () {
+    $(".toast").hide();
+  })
 
 $("#search-form").on("submit", function(event){
     
@@ -8,6 +12,7 @@ $("#search-form").on("submit", function(event){
     var searchSerial = $('#search-form').serialize(); // search=dora%20the
 
     if($("#search-form input").val() == ""){
+        $(".toast").show();
         $('.toast').toast('show');
     }
     else {
